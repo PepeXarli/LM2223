6b82854ae45a4664c4ef4960a394a779afe0653e
José Carlos Navarro <josenavarro04@iesamachado.org>
2023-01-12 17:43:38 +0100

    finalizo ejercicios JavaScript 1

---
+++ EtiquetasSmples/EjerciciosJavaScript/circulos.js
@@ -0,0 +1,18 @@
+function norojo() {
+    document.getElementById('rojo').style.display='none'
+}
+function rojo(){
+    document.getElementById('rojo').style.display='block'
+}
+function noazul() {
+    document.getElementById('azul').style.display='none'
+}
+function azul(){
+    document.getElementById('azul').style.display='block'
+}
+function noyellow() {
+    document.getElementById('yellow').style.display='none'
+}
+function yellow(){
+    document.getElementById('yellow').style.display='block'
+}
