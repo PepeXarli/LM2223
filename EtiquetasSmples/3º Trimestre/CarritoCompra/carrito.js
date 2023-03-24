5bb7a72958c33fcbc01d98ec844e7392e2155cd3
José Carlos <josenavarro04@iesamachado.org>
2023-03-24 14:25:29 +0100

    vamo a intentar q funcione

--- EtiquetasSmples/3º Trimestre/CarritoCompra/carrito.js
+++ EtiquetasSmples/3º Trimestre/CarritoCompra/carrito.js
@@ -1,7 +1,21 @@
+var carro = [
+    {id:0, titulo:'teclado', precio:34.90, uds:0},
+    {id:1, titulo:'raton', precio:109.89, uds:0},
+    {id:2, titulo:'monitor', precio:228.60, uds:0},
+    {id:3, titulo:'disco', precio:28.99, uds:0},
+    {id:4, titulo:'movil', precio:1289.00, uds:0},
+    {id:5, titulo:'altavoz', precio:119.99, uds:0},
+
+
+]
+
+
 function sumar(a){
-    b=parseInt(document.getElementById(a).innerHTML);
-    b++;
-    document.getElementById(a).innerHTML=b;
+
+    let producto=carro[a];
+    producto['uds']++;
+    carro[a]=producto;
+    document.getElementById('id'+a).value=producto['uds'];
 }
 /*
 function sumar(a){
