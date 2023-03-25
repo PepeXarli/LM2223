e55d134ee5665bc1dccf440ea8a2e70f9e251e80
José Carlos Navarro <josenavarro04@iesamachado.org>
2023-03-25 14:46:52 +0100

    cesta casi casi casi funcional

--- EtiquetasSmples/3º Trimestre/CarritoCompra/carrito.js
+++ EtiquetasSmples/3º Trimestre/CarritoCompra/carrito.js
@@ -1,13 +1,14 @@
 var carro = [
-    {id:0, titulo:'teclado', precio:34.90, uds:0},
-    {id:1, titulo:'raton', precio:109.89, uds:0},
-    {id:2, titulo:'monitor', precio:228.60, uds:0},
-    {id:3, titulo:'disco', precio:28.99, uds:0},
-    {id:4, titulo:'iphone', precio:1289.00, uds:0},
-    {id:5, titulo:'altavoz', precio:119.99, uds:0},
+    {id:0, titulo:'teclado', precio:34.90, uds:0, img:'0.png'},
+    {id:1, titulo:'raton', precio:109.89, uds:0, img:'1.png'},
+    {id:2, titulo:'monitor', precio:228.60, uds:0, img:'2.png'},
+    {id:3, titulo:'disco', precio:28.99, uds:0, img:'3.png'},
+    {id:4, titulo:'iphone', precio:1289.00, uds:0, img:'4.png'},
+    {id:5, titulo:'altavoz', precio:119.99, uds:0, img:'5.png'},
 
 ]
 
+iniciar();
 
 function sumar(a){
     let producto=carro[a];
@@ -52,14 +53,32 @@ function refrescar(){
         if(producto['uds']>0){
             let nodo=document.createElement('div');
             let img=document.createElement('img');
+            let span1=document.createElement('span');
+            let span2=document.createElement('span');
             img.src=i+'.png';
-            let texto=document.createTextNode(' '+producto['precio']+' '+ producto['uds']);
+            span1.innerHTML=' '+producto['precio'].toFixed(2)+'€ ';
+            span2.innerHTML=' '+producto['uds'];
             document.getElementById('id'+i).innerHTML=producto['uds'];
-            let full=img+texto
-            nodo.appendChild(full);
+            nodo.appendChild(img);
+            nodo.appendChild(span1);
+            nodo.appendChild(span2);
             listaCompra.appendChild(nodo);
             total=total+producto['uds']*producto['precio'];
         }
     }
-    document.getElementById('total').innerHTML = 'Total: ';
+    document.getElementById('total').innerHTML = 'Total: ' + total.toFixed(2)+'€';
 }
+function iniciar(){
+    let almacenado=localStorage.getItem('carro');
+        if(almacenado!=null){
+            almacenado=JSON.parse(almacenado);
+            carro=almacenado;
+            for(let i=0; i<carro.length; i++){
+                let producto=carro[i];
+                document.getElementById('id'+i).innerHTML=producto['uds'];
+            }
+            refrescar();
+    }
+}
+
+
